07c09b316ad04be112c17ac27f0fc8ae76c64e11
Алексей Спасюк <spasiuk@overdrive.by>
2020-02-19 23:30:31 +0300

    feat: all tests is pass

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 
-exports.min = function min (array) {
+exports.min = function min (array = []) {
+  if (array.length == 0 || array == undefined) return 0;
   return array.sort((a,b)=>(a-b))[0];
 }
 
-exports.max = function max (array) {
-  return array.sort((a,b)=>(b-a))[0];
+exports.max = function max (array = []) {
+  if (array.length == 0 || array == undefined) return 0;
+  return (!!array)?array.sort((a,b)=>(b-a))[0]:0;
 }
 
-exports.avg = function avg (array) {
-  let result = arr.reduce((sum, current) => sum + current, 0);
-  result /=array.length;
-  return result;
+exports.avg = function avg (array = []) {
+  if (array.length == 0 || array == undefined) return 0;
+  let sum = 0;
+  array.forEach(item=>sum+=item);
+  return sum/array.length;
 }
