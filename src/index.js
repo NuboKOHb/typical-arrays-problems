5fd80c4955590fb09ac8b0f662b31e13dcacc090
Алексей Спасюк <aleksei.spasiuk@yandex.ru>
2020-02-19 23:12:42 +0300

    feat: more tests is pass

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array.sort((a,b)=>(a-b))[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array.sort((a,b)=>(b-a))[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = arr.reduce((sum, current) => sum + current, 0);
+  result /=array.length;
+  return result;
 }
